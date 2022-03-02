aaaf5d77e2595b56ef9adc00e2332bd87d4584d8
jllv <drake@pku.edu.cn>
2022-03-02 16:37:28 +0800

    add chisel original project

---
+++ chisel/src/main/scala/adder.scala
@@ -0,0 +1,16 @@
+// adder.scala
+package test
+ 
+import chisel3._
+ 
+class Adder(n: Int) extends Module {
+  val io = IO(new Bundle {
+    val a = Input(UInt(n.W))
+    val b = Input(UInt(n.W))
+    val s = Output(UInt(n.W))
+    val cout = Output(UInt(1.W))  
+  })
+ 
+  io.s := (io.a +& io.b)(n-1, 0)
+  io.cout := (io.a +& io.b)(n)
+}
