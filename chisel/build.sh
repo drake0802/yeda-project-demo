676bc1c7c10fa1ccc53a7c7ed5e388a02c6d71c3
jllv <drake@pku.edu.cn>
2022-03-02 17:11:02 +0800

    refactor

--- chisel/build.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-rm ./generated/ ./project/ ./target/ -rf
-sbt 'test:runMain test.AdderTestGen -td ./generated/addertest --backend-name verilator' 
-
-vcd2wavedrom --input ./generated/addertest/*/Adder.vcd --exclude exclude.txt --include include.txt --output wave.drom
--- chisel/src/main/scala/adderGen.scala
+++
@@ -1,6 +0,0 @@
-// adderGen.scala
-package test
- 
-object AdderGen extends App {
-  chisel3.Driver.execute(args, () => new Adder(args(0).toInt))
-}
